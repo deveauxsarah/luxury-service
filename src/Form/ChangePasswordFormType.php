112e3ab84562c2239b2c65c2a07a8ba0f8ba8b9b
mlp-dw <mlp.dwwb@gmail.com>
2022-05-04 09:18:45 +0200

    08 - reset password OK

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -15,23 +15,25 @@ class ChangePasswordFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', RepeatedType::class, [
+            ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
                     'constraints' => [
                         new NotBlank([
                             'message' => 'Please enter a password',
                         ]),
                         new Length([
-                            'min' => 8,
-                            'minMessage' => 'Your password should be at least 8 characters',
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
                             // max length allowed by Symfony for security reasons
-                            'max' => 255,
+                            'max' => 4096,
                         ]),
                     ],
                     'label' => 'New password',
                 ],
                 'second_options' => [
+                    'attr' => ['autocomplete' => 'new-password'],
                     'label' => 'Repeat Password',
                 ],
                 'invalid_message' => 'The password fields must match.',
@@ -39,7 +41,6 @@ class ChangePasswordFormType extends AbstractType
                 // this is read and encoded in the controller
                 'mapped' => false,
             ])
-            ->add('confirmPassword', PasswordType::class)
         ;
     }
 
