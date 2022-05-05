80a3c18fcb499b724f21392488ddfca36e0eaf67
mlp-dw <mlp.dwwb@gmail.com>
2022-05-05 11:14:55 +0200

    11 - filter offer categories OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Offer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $offers = $entityManager
+            ->getRepository(Offer::class)
+            ->findAll();
+
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'offers' => $offers,
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
